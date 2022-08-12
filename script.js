7c2685fb0741c5870df0227d0478e70d3d76b8b9
Scott Simon <simon_phd@protonmail.com>
2022-08-11 21:36:52 -0500

    Added assignment text to readme

--- script.js
+++ script.js
@@ -4,10 +4,12 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Write password to the #password input - All code goes in here
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
+  // All code goes in here
 
   passwordText.value = password;
 
