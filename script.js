84c014cd65cf2c5ba11edf174a16fbcdb42ef470
Scott Simon <simon_phd@protonmail.com>
2022-08-14 07:04:43 -0500

    Added more prompts

--- script.js
+++ script.js
@@ -6,9 +6,12 @@ var generateBtn = document.querySelector("#generate");
 // 1. Prompt user for critera (pw length, char type)
 // 2. Validate input (between 8 - 128 and at least one char type)
 // 3. Generate pw
-var userChoice = window.prompt("Chose length between 8 and 120 characters");
 
-
+var userChoice1 = window.prompt("Chose length between 8 and 120 characters");
+var userChoice2 = window.prompt("Chose if lower case")
+var userChoice3 = window.prompt("Chose if upper case");
+var userChoice4 = window.prompt("Chose if number");
+var userChoice5 = window.prompt("Chose if special character");
 
 // 4. Display pw
 
