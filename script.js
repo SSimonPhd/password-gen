16c41155bcc5bff660e85d021ddb6763f190aee0
Scott Simon <simon_phd@protonmail.com>
2022-08-14 05:52:53 -0500

    Some initial variables based on miniproject

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 // Assignment code here
+var pwLength = 8;
+var charType = "Lower Case";
 
+var lengthOptions = [8 - 128];
+
+var typeOptions = ["Upper case", "Lower case", "Number", "Special Character"];
+
+var generatePassword = function() {
+  // Ask user for their choice
+  var userChoice = window.prompt("Enter length 8 - 120:");
+
+  // If user pressed Cancel, immediately end function
+  if (!userChoice) {
+    return;
+  } 
+  else { 
+    
+  var userChoice2 = window.prompt("Enter character type:");
+  }
+}
+  // Get random index from array of options
+  var index = Math.floor(Math.random() * options.length);
+  var computerChoice = options[index];
+
+  window.alert("The computer chose " + computerChoice);
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
